bad02786ad2c89eb06395f290dd542b63ecb3a00
Chinopas MUANGTHAI <chinopas.m@live.ku.th>
2022-02-16 16:24:32 +0700

    Add project files.

---
+++ Test_repo_PRS/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Test_repo_PRS.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Test_repo_PRS.Models;
+
+namespace Test_repo_PRS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Esignature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Savedata(ModelFormTOR modelform)
+        {
+            string filesig = modelform.Esignature;
+            string uniquefile = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string fileName = modelform.Username + "_" + uniquefile + ".png";
+            string filePath = Path.Combine(_hostingEnvironment.WebRootPath, "image", fileName); //image คือ พาทRoot ของ image โดยเซฟเป็นชื่อ filename
+            System.IO.File.WriteAllBytes(filePath, Convert.FromBase64String(modelform.Esignature.Replace("data:image/png;base64,", string.Empty)));
+
+            return View();
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Test_repo_PRS/Models/Data/ModelFormTOR.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Test_repo_PRS.Models.Data
+{
+    public class ModelFormTOR
+    {
+        public string Esignature { get; set; }
+        public string Username { get; set; }
+    }
+}
